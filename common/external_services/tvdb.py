d51e4bacbdf1b8b3efd470aa3c0eaa2e4d1404cb
harment <98186868+harment@users.noreply.github.com>
2026-05-08 16:47:28 +0300

    Move TVDB PIN to configuration

--- common/external_services/tvdb.py
+++ common/external_services/tvdb.py
@@ -6,7 +6,10 @@ from unit3dup import config_settings
 class TVDB:
 
     def __init__(self, category: str):
-        self.api = tvdb_v4_official.TVDB(config_settings.tracker_config.TVDB_APIKEY, pin="SXVUP0VA")
+        self.api = tvdb_v4_official.TVDB(
+            config_settings.tracker_config.TVDB_APIKEY,
+            pin=config_settings.tracker_config.TVDB_PIN
+        )
         self.category = category
         self.filtered_results = []
 
